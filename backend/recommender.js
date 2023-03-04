bfb4ec24d9987aff84123b75716b5252f7de3d9c
Avinash Yadav <avinashyadav.15@outlook.com>
2023-03-05 01:00:10 +0530

    server, mvc

--- backend/recommender.js
+++ backend/recommender.js
@@ -1,9 +1,11 @@
 const fs = require("fs")
 const { parse } = require("csv-parse")
+const { Restaurants } = require("./restaraunts")
+
 fs.createReadStream("D://one.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
-    console.log(row);
+    Restaurants.create({rid : parseInt(row[0]) ,name : row[1] , latitude : parseFloat(row[2]) , longitude : parseFloat(row[3]) , cuisine : row[4] , price : parseInt(row[5])})
 })
 var data = fs
 module.exports = {data}
