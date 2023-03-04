dc0cb6cbe88eecd7912d334f52e3e1eb6c62d485
arsh73552 <arsh01042002@gmail.com>
2023-03-04 20:43:20 +0530

    Calculate Distance between person and restaurant

---
+++ backend/calculateDistance.js
@@ -0,0 +1,15 @@
+function distanceCalculator(lat1, lon1, lat2, lon2)
+{
+    const R = 6371e3; 
+    const φ1 = lat1 * Math.PI/180; 
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (lon2-lon1) * Math.PI/180;
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    const d = R * c;
+}
+
+module.exports = {distanceCalculator};
