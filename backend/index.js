285c2c98269c32681640ab1c950f06f841007426
Avinash Yadav <avinashyadav.15@outlook.com>
2023-03-04 16:57:08 +0530

    restaurants table

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 (async () => {
   try {
-    const [results, metadata] = await sequelize.query("Show tables");
+    const [results, metadata] = await sequelize.query("CREATE TABLE users (name varchar(255), age integer, phone_number varchar(20),lat float, longit float ,email_id varchar(255),preferences varchar(255)[])");
     console.log(results);
   } catch (err) {
     console.error("error executing query:", err);
