be20559edd0fa557ced38aabc6f0e0c05e6cf7b8
Avinash Yadav <avinashyadav.15@outlook.com>
2023-03-06 20:30:09 +0530

    fixed restaurants

--- frontend/src/pages/restraunt.js
+++ frontend/src/pages/restraunt.js
@@ -1,48 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Typography from '@mui/material/Typography';
 import './style.css';
 import { Button, ButtonGroup } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import ActionCard from '../components/card'
 import background from "../images/background.png";
-<<<<<<< HEAD
-import { useState , useEffect } from 'react';
-export function Restraunt() {
-    const [rdata , setRdata] = useState(null)
-    const fetchRdata = async () => {
-    try {
-        const response = await fetch('localhost:8080/restaurants');
-        const data = await response.json();
-        setRdata(data);
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-    };
-
-    useEffect(() => {
-    fetchRdata();
-    }, [])
-
-    
-=======
 import {OrderFood} from "./orderFood"
-import { useState } from 'react';
-
 export function Restraunt() {
-
-    const [restaurantName, setrestrauntName] = useState("Chillis")
-
+    
     function menu() {
         const root = ReactDOM.createRoot(document.getElementById('root'));
-        setrestrauntName()
         root.render(
         <React.StrictMode>
             <OrderFood />
          </React.StrictMode>
 );
     }
-
-
     
     const data = [
         {
@@ -51,52 +25,51 @@ export function Restraunt() {
             price: 2000
         },
         {
-            restrauntName: 'Nandos',
-            distance: 20,
-            price: 200
-        },
-        {
-            restrauntName: 'Starbucks',
+            restrauntName: 'Indo Cafe',
             distance: 50,
-            price: 4000
+            price: 2000
         },
         {
-            restrauntName: 'CCD',
-            distance: 20,
-            price: 500
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
         {
-            restrauntName: 'KFC',
-            distance: 80,
-            price: 500
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
         {
-            restrauntName: 'Big Chill',
-            distance: 100,
-            price: 4000
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
         {
-            restrauntName: 'Noryak',
-            distance: 30,
-            price: 400
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
         {
-            restrauntName: 'Burger King',
-            distance: 30,
-            price: 600
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
         {
-            restrauntName: 'Aryan',
-            distance: 10,
-            price: 3000
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
         {
-            restrauntName: 'Chillis',
-            distance: 40,
-            price: 800
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
+        },
+        {
+            restrauntName: 'Indo Cafe',
+            distance: 50,
+            price: 2000
         },
     ]
->>>>>>> 6efa2c09cd271b1e086b276a2c4601804df7899c
     return (
         <div className='LandingPage' style={{
             backgroundImage: `url(${background})`,
@@ -117,15 +90,10 @@ export function Restraunt() {
                     <div className='msg' style={{ height: '64px', order: 0 }}>Okay, so you like a close-by restaurant with Indian cuisine which is moderately expensive.</div>
                     <div className='msg' style={{ height: '42px', order: 1 }}>Here are some personalised suggestions for you: </div>
                     <div className='card-container' style={{order: 2 }}>
-<<<<<<< HEAD
-                        {rdata.map((i, index) => (
-                            <ActionCard restrauntName={i.name} distance={i.cuisine} price={i.price} />
-=======
                         {data.map((i, index) => (
-                            <span onClick={(menu)} onChange>
+                            <span onClick={(menu)}>
                                 <ActionCard restrauntName={i.restrauntName} distance={i.distance} price={i.price} />
                             </span>
->>>>>>> 6efa2c09cd271b1e086b276a2c4601804df7899c
                         ))}
                     </div>
                 </div>
