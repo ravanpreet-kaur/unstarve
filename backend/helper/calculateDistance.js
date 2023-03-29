9e7e1a389896cf5d89e0a3d73528c360911a8cb5
Avinash Yadav <avinashyadav.15@outlook.com>
2023-03-29 05:58:59 +0530

    added jest for backend

--- backend/helper/calculateDistance.js
+++ backend/helper/calculateDistance.js
@@ -1,5 +1,4 @@
-function distanceCalculator(lat1, lon1, lat2, lon2)
-{
+function distanceCalculator(lat1, lon1, lat2, lon2){
     return Math.abs(lat1 - lat2) + Math.abs(lon1 - lon2);
 }
 
--- backend/tests/distance.test.js
+++ backend/tests/distance.test.js
@@ -1,5 +1,5 @@
 const { distanceCalculator } = require("../helper/calculateDistance");
 
-test('finds distance between Chandigarh and Mumbai' , () => {
+test('finds distance between (1 , 1) and (0 , 0)' , () => {
     expect(distanceCalculator(1 , 1 , 0 , 0)).toBe(2);
 })
